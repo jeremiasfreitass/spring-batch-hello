4a6abad6e6852ed995968c0b3dc26060547cf2c5
Jeremias Freitas <jeremias.freitas@dtidigital.com.br>
2023-12-17 19:13:44 -0300

    hello word utilizand spring batch

---
+++ src/main/java/com/udemy/primeiroprojetobatch/BatchConfig.java
@@ -0,0 +1,33 @@
+package com.udemy.primeiroprojetobatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+@Configuration
+public class BatchConfig {
+    @Bean
+    public Job imprimeOlaJob(JobRepository jobRepository, Step imprimeOlaStep){
+        return new JobBuilder("job", jobRepository)
+                .start(imprimeOlaStep)
+                .build();
+    }
+    @Bean
+    public Step imprimeOlaStep(JobRepository jobRepository, PlatformTransactionManager transactionManager){
+        return new StepBuilder("step", jobRepository)
+                .tasklet((StepContribution contribution, ChunkContext chunkContext) -> {
+                    System.out.println("Olá Mundo");
+                    return RepeatStatus.FINISHED;
+                },  transactionManager)
+                    .build();
+    }
+}
---
+++ src/main/java/com/udemy/primeiroprojetobatch/PrimeiroprojetobatchApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.primeiroprojetobatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroprojetobatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroprojetobatchApplication.class, args);
+	}
+
+}
